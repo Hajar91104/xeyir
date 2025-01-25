0b736a15c777576ad7123d62da2ebcc8ef6dbf69
Hajar91104 <hajaram@code.edu.az>
2025-01-25 16:00:45 +0400

    feat:add tailwind & shadcn

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import "./style/global.css";
+
+createRoot(document.getElementById("root")!).render(
+  <div className="text-green-500">Hello</div>
+);
